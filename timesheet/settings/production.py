9b9870a107ed2822353513be303e8d30ff2139cc
Lenin <lenin.aguilar@gmail.com>
2018-10-18 22:27:36 -0400

    Update no sql

--- timesheet/settings/production.py
+++ timesheet/settings/production.py
@@ -2,3 +2,9 @@ from .base import *
 
 DEBUG = False
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'prd_db.sqlite3'),
+    }
+}
