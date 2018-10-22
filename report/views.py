d2ab9e384b0f9229bfb555b757548659f61d4e35
Lenin <lenin.aguilar@gmail.com>
2018-10-22 00:05:12 -0400

    Update report/views.py

--- report/views.py
+++ report/views.py
@@ -181,10 +181,16 @@ def WeekDetailView(request, pk):
     if print_option == "yes":
         context = {'week': week, 'comments': comments, 'miscelaneous': miscelaneous, 'sent': sent, 'name': name, 'days_in_week': days_in_week, 'hours': hours, 'week_id': week_id}
         content = render_to_string('print_week.html', context)
-        pdf_file = HTML(string=content).write_pdf('./tmp.pdf', stylesheets=[CSS('./static/stylesheet.css')])
+        HTML(string=content).write_pdf('./tmp.pdf', stylesheets=[CSS('./static/stylesheet.css')])
 
-        response = HttpResponse(pdf_file, content_type='application/pdf')
-        return response
+        #response = HttpResponse(pdf_file, content_type='application/pdf')
+        #return response
+
+        with open('./tmp.pdf', 'r') as pdf:
+            response = HttpResponse(pdf.read(), mimetype='application/pdf')
+            pdf.closed
+            return response
+        
 
     else:
         context = {'week':week, 'comments':comments, 'miscelaneous': miscelaneous, 'sent': sent, 'name': name, 'days_in_week': days_in_week, 'hours': hours, 'week_id': week_id}
