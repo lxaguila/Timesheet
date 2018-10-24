c0375a4b9f1fe5e97707500ae590678b1e87524f
Lenin <lenin.aguilar@gmail.com>
2018-10-24 17:54:43 -0400

    www

--- timesheet/settings/base.py
+++ timesheet/settings/base.py
@@ -77,9 +77,15 @@ WSGI_APPLICATION = 'timesheet.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'prd_db.sqlite3'),
     }
 }
+
+
+#        'NAME': os.path.join(BASE_DIR, 'prd_db.sqlite3'),
+
+
 
 
 # Password validation
