ca2a696aa2af6d61d68f16b73d507108cd557315
Lenin <lenin.aguilar@gmail.com>
2018-10-26 18:18:59 -0400

    Update timesheet/settings/base.py

--- timesheet/settings/base.py
+++ timesheet/settings/base.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'report.apps.ReportConfig'
+    'report.apps.ReportConfig',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
